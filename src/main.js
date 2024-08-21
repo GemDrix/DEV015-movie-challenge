64db696ee895d2f76b46d86dbfdc77c86ccb58aa
Gema Riffo <gema.r.y382@gmai.com>
2024-08-21 14:16:04 -0400

    avance final de historiade usuario 1 lista de peliculas

--- src/main.js
+++ src/main.js
@@ -1,15 +1,7 @@
-
 import dataset from '../data/dataset.js';
 import { renderItems } from './components/App.js';
 
+const tarjeta = renderItems(dataset)
 
-
-const tarjetas = renderItems(dataset)  // Llama a "renderItems" para crear las tarjetas a partir de los datos
-const TotalTarjetas = document.getElementById("peliculas") // se obtiene el elemento del DOM donde se agregarán las tarjetas
-tarjetas.forEach(tarjeta => {           // Añade cada tarjeta (ul) al contenedor en el DOM
-  TotalTarjetas.appendChild(tarjeta)
-});
-
-
-
-
+document.getElementById("cartelera").innerHTML = tarjeta;
+console.log(tarjeta);
