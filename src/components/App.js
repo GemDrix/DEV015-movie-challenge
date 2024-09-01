70ade204f5909eaf921ded36d08150edc0555f8c
Gema Riffo <gema.r.y382@gmai.com>
2024-09-01 04:07:55 -0400

    Implementación ventana modal, dando funcionalidad a los filtros con el modal

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 //Creacion de divs con los datos a mostrar.
 export const renderItems = (dataset) => {
   const tarjeta = dataset.map(pelicula => `
-      <div class="contenedor">
-        <img src= "https://image.tmdb.org/t/p/original${pelicula.poster_path}" alt="${pelicula.title}">
-         <h2>${pelicula.title}</h2>
-         <h3 class = "fecha">${pelicula.release_date}</h3>
+      <div class="contenedor" id=${pelicula.id}>
+        <img class= tarjetaImg src= "https://image.tmdb.org/t/p/original${pelicula.poster_path}" alt="${pelicula.original_title}">
+         <h2 class= infoTarjeta>${pelicula.original_title}</h2>
+         <h3 class= infoTarjeta>Fecha Estreno: ${pelicula.release_date}</h3>
       </div>
     `).join('');
   return tarjeta;
 };
+
 
 export const filterData = (dataset, filterBy, value) => {                           //Exporta la funcion para filtrar por genero
   const dataFiltrada = dataset.filter(pelicula => pelicula.genres.includes(value)); //Indica donde buscar dentro de la dataset para filtrar
@@ -20,10 +21,53 @@ export const sortData = (data, sortBy, sortOrder) => {
     if (a[sortBy] > b[sortBy]) {
       return sortOrder === 'asc' ? 1 : -1;
     } else if (a[sortBy] < b[sortBy]) {
-      return sortOrder === 'asc' ? -1 : 1;
+      return sortOrder === '' ? -1 : 1;
     } else {
       return 0;
     }
   };
   return data.slice().sort(comparator); // Ordenar usando el comparador
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const sortData = (data, sortBy, sortOrder) => {
+//   const dataOrdenada = data
+//   if (sortOrder === "asc") {
+//     return dataOrdenada.sort((a, b) => {
+//       if (a[sortBy] > b[sortBy]) {
+//         return 1;
+//       } else if (a[sortBy] < b[sortBy]) {
+//         return -1;
+//       } else {
+//         return 0;
+//       }
+//     });
+//   } else if (sortOrder === "desc") {
+//     return dataOrdenada.sort((a, b) => {
+//       if (a[sortBy] > b[sortBy]) {
+//         return -1;
+//       } else if (a[sortBy] < b[sortBy]) {
+//         return 1;
+//       } else {
+//         return 0;
+//       }
+//     });
+//   } else {
+
+//     return dataOrdenada;
+//   }
+// };
