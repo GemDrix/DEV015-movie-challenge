c0ad50c5aaca52677aa420ce0b99100aeba2ce2e
Gema Riffo <gema.r.y382@gmai.com>
2024-09-09 02:37:22 -0300

    filtros operativos, con la implementación de la API

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,21 @@
+export const filterData = (respuesta, filterBy, value) => {                           //Exporta la funcion para filtrar por genero
 
+  const idGenero = parseInt(value, 10);
 
+  const dataFiltrada = respuesta.filter(pelicula =>
+    pelicula[filterBy] && pelicula[filterBy].includes(idGenero)
+  );
 
-
-
-
-
-//Creacion de divs con los datos a mostrar.
-// export const renderItems = (dataset) => {
-//   const tarjeta = dataset.map(pelicula => `
-//       <div class="" id=${pelicula.id}>
-//         <img class= "tarjetaImg" src= "https://image.tmdb.org/t/p/original${pelicula.poster_path}" alt="${pelicula.original_title}">
-//          <h2 class= infoTarjeta>${pelicula.original_title}</h2>
-//          <h3 class= infoTarjeta>Fecha Estreno: ${pelicula.release_date}</h3>
-//       </div>
-//     `).join('');
-//   return tarjeta;
-// };
-
-
-export const filterData = (dataset, filterBy, value) => {                           //Exporta la funcion para filtrar por genero
-  const dataFiltrada = dataset.filter(pelicula => pelicula.genres.includes(value)); //Indica donde buscar dentro de la dataset para filtrar
-  return dataFiltrada;                                                              //Regresa la data filtrada
+  return dataFiltrada;
 };
 
 export const sortData = (data, sortBy, sortOrder) => {
-  const comparator = (a, b) => {
-    if (a[sortBy] > b[sortBy]) {
-      return sortOrder === 'asc' ? 1 : -1;
-    } else if (a[sortBy] < b[sortBy]) {
-      return sortOrder === 'asc' ? -1 : 1;
-    } else {
-      return 0;
-    }
-  };
-  return data.slice().sort(comparator); // Ordenar usando el comparador
+  return data.slice().sort((a, b) => {
+    const dateA = new Date(a[sortBy]);
+    const dateB = new Date(b[sortBy]);
+
+    if (dateA > dateB) return sortOrder === 'asc' ? 1 : -1;
+    if (dateA < dateB) return sortOrder === 'asc' ? -1 : 1;
+    return 0;
+  });
 };
-
-
-
-
